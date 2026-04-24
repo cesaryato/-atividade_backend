3363227c234dc35736fdd2332b59cda4ab43edae
cesaryato <148984460+cesaryato@users.noreply.github.com>
2026-04-24 10:26:04 -0300

    Add files via upload

--- Formulario/index.php
+++ Formulario/index.php
@@ -87,6 +87,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             ?>
          </div>
     </div>
+    <?php endif; ?>
+    <h2 style="margin-top: 35px;">Usuários cadastrados</h2>
     <?php if ($resultLista && pg_num_rows($resultLista) > 0): ?>
       <table>
         <thead>
