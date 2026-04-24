ca65e0d7b719d33dc50bd50d50f7f21e19b265f7
cesaryato <148984460+cesaryato@users.noreply.github.com>
2026-04-24 10:04:42 -0300

    Add files via upload

--- Formulario/index.php
+++ Formulario/index.php
@@ -1,3 +1,55 @@
+<?php
+$nome= "";
+$email= "";
+$telefone= "";
+$mensagem= "";
+$tipoMensagem= "";
+
+$db_url = getenv("DATABASE_URL"); 
+if (!$db_url) { 
+    die("Erro: variável DATABASE_URL não encontrada.");
+
+}
+$conn = pg_connect($db_url);
+
+if (!$conn) {
+    die("Erro ao conectar no banco de dados")
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nome = trim($_POST["nome"] ?? "");
+    $email = trim($_POST["email"] ?? "");
+    $telefone = trim($_POST["telefone"] ?? "");
+
+    if ($nome === "" || $email === "" || $telefone === "") {
+        $mensagem = "Preencha todos os campos.";
+        $tipoMensagem = "erro";
+    } elseif ($filter_var($email, FILTER_VALID_EMAIL)) {
+        $mensagem = "Digite um e-mail válido.";
+        $tipoMensagem = "erro";
+    } else{
+        $query = "INSERT INTO usuarios (nome, email, telefone) VALUES ($1, $2, $3)";
+        $result = pg_query_params($conn, $query, [$nome, $email, $telefone]);
+
+        if($result) {
+           $mensagem = "Usuário cadastrado com sucesso no banco de dados.";
+           $tipoMensagem = "sucesso";
+
+           $nome = "";
+           $email = "";
+           $telefone = "";
+
+        }else{
+            $mensagem = "Erro ao salvar no banco de dados.";
+            $tipoMensagem = "erro";
+
+        }
+    }
+}
+$queryLista = "SELECT id, nome, email, telefone FROM usuarios ORDER BY id DESC";
+$resultLista = pg_query($conn, $queryLista);
+?> 
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
